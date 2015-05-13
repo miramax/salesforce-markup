4088339cefe03e0191f59b6e6a2199e9a7f1d9e9
Andrey Gradusov <m1ramax@yandex.ru>
2015-05-13 15:02:18 +0300

    add promo list

--- assets/js/promo.js
+++ assets/js/promo.js
@@ -1,5 +1,40 @@
 $(document).ready(function() {
-    console.log('promo is loaded');
+    var sorting = {};
+
+    render('#promoItem', '.promo-items', JsonData.promotions);
+
+    $('#business_types, #product_types').on('change', function(e) {
+        var bType = +$('#business_types').val(),
+            pType = +$('#product_types').val(),
+            items;
+
+        // clean previous
+        sorting = {};
+
+        if(pType !== 1) {
+            sorting.product_type = pType;
+        }
+        if(bType !== 1) {
+            sorting.business_type = bType;
+        }
+
+        items = _.filter(JsonData.promotions, typeSorter);
+
+        render('#promoItem', '.promo-items', items);
+    });
+
+    function typeSorter(item) {
+        var key;
+
+        for(key in sorting) {
+            if ( typeof item.types[key] === 'undefined' ||
+                 item.types[key] !== sorting[key] ) {
+                return false;
+            }
+        }
+
+        return item;
+    }
 });
 
 function render(templateId, container, items) {
