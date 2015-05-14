8f513c01fc930c36d299b8b3d7438c809d62aa27
Andrey Gradusov <m1ramax@yandex.ru>
2015-05-14 17:06:09 +0300

    change select options sorting method

--- assets/js/promo.js
+++ assets/js/promo.js
@@ -1,40 +1,49 @@
 $(document).ready(function() {
     $('html').addClass('gray');
-    var sorting = {};
+    var sorting,
+        ALL_OPTION = 'all';
 
+    // paste default items
     render('#promoItem', '.promo-items', JsonData.promotions);
+    appendOptions('#product_types', JsonData.selectOptions.product);
+    appendOptions('#business_types', JsonData.selectOptions.business);
 
     $('#business_types, #product_types').on('change', function(e) {
-        var bType = +$('#business_types').val(),
-            pType = +$('#product_types').val(),
+        var bType = $('#business_types').val(),
+            pType = $('#product_types').val(),
             items;
 
         // clean previous
         sorting = {};
 
-        if(pType !== 1) {
+        if(pType !== ALL_OPTION) {
             sorting.product_type = pType;
         }
-        if(bType !== 1) {
+        if(bType !== ALL_OPTION) {
             sorting.business_type = bType;
         }
 
-        items = _.filter(JsonData.promotions, typeSorter);
+        items = _.filter(JsonData.promotions, sortingMethod);
 
         render('#promoItem', '.promo-items', items);
     });
 
-    function typeSorter(item) {
+    function sortingMethod(item) {
         var key;
 
         for(key in sorting) {
-            if ( typeof item.types[key] === 'undefined' ||
-                 item.types[key] !== sorting[key] ) {
+            if ( typeof item[key] === 'undefined' ||
+                 item[key] !== sorting[key] ) {
                 return false;
             }
         }
 
         return item;
+    }
+
+    function appendOptions(container, items) {
+        var template = _.template($('#selectOptions').html());
+        $(container).append(template({items: items}));
     }
 });
 
