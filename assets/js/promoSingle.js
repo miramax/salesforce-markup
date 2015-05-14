ab77d08e315be87ffc0fcccdd130260b0193a87e
Andrey Gradusov <m1ramax@yandex.ru>
2015-05-14 12:22:48 +0300

    promo single

--- assets/js/promoSingle.js
+++ assets/js/promoSingle.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
+    // paste content
+    $('.black-header__title').html(JsonData.title);
+    $('.black-header__util span').html(JsonData.util);
+    $('.section-details').html(JsonData.details);
+    $('.section-tc').html(JsonData.tc);
+    render('#resource', '.section-resources', JsonData.resources);
+
+    $('.nav-tabs li a').on('click', function() {
+        var el = $(this);
+        if(el.data('tab') === 'resources') {
+            $('html').addClass('gray');
+        } else {
+            $('html').removeClass('gray');
+        }
+    });
 });
 
 function render(templateId, container, items) {
